538407595d1b039814e2bf40b51011696185b1f3
Changjun Yeo <lunaradio24@gmail.com>
2024-07-08 11:59:37 +0900

    Feat: 거래기록 조회 #8

--- src/transaction-log/transaction-log.controller.ts
+++ src/transaction-log/transaction-log.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { TransactionLogService } from './transaction-log.service';
+import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { Roles } from 'src/utils/decorators/roles.decorator';
 
-@Controller('transaction-log')
+@Controller('transaction-logs')
 export class TransactionLogController {
   constructor(private readonly transactionLogService: TransactionLogService) {}
 
   @Get()
+  @UseGuards(AccessTokenGuard, RoleGuard)
+  @Roles(['ADMIN'])
   async findAll() {
     return this.transactionLogService.findAll();
   }
