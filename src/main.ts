ffc0f77805c0392e4fcd87409b058e90b709806b
Changjun Yeo <lunaradio24@gmail.com>
2024-07-08 12:05:05 +0900

    Feat: logging interceptor #9

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpErrorFilter } from './common/exception-filters/http-error.filter';
 import { ConfigService } from '@nestjs/config';
+import { LoggingInterceptor } from './common/interceptors/logging/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpErrorFilter());
   await app.listen(new ConfigService().get('SERVER_PORT'));
 }
