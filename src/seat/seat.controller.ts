5123a71d46ce48cba14ed50548282761e758b4f9
Changjun Yeo <lunaradio24@gmail.com>
2024-07-08 11:54:09 +0900

    Feat: 예매 가능한 좌석 조회 #6

--- src/seat/seat.controller.ts
+++ src/seat/seat.controller.ts
@@ -1,22 +1,12 @@
-import { Controller, Get, Patch, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { SeatService } from './seat.service';
 
 @Controller('seats')
 export class SeatController {
   constructor(private readonly seatService: SeatService) {}
 
-  @Get()
-  findAll() {
-    return this.seatService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.seatService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.seatService.update(+id);
+  @Get('showDate/:showDateId')
+  findAllAvailable(@Param('showDateId', ParseIntPipe) showDateId: number) {
+    return this.seatService.findAllAvailable(showDateId);
   }
 }
--- src/seat/seat.module.ts
+++ src/seat/seat.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeatService } from './seat.service';
 import { SeatController } from './seat.controller';
+import { Seat } from './entities/seat.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Seat])],
   controllers: [SeatController],
   providers: [SeatService],
 })
